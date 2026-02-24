2ae4e87a7a91856ce393bf2a2d7cb538081ab82e
Mike Bailey <michael.l.bailey@hotmail.com>
2026-02-24 10:00:50 -0500

    Update asm_macros.h

--- spec/asm_macros.h
+++ spec/asm_macros.h
@@ -28,117 +28,43 @@
 #ifndef ASM_MACROS_H
 #define ASM_MACROS_H
 
-// delay macros
-__asm__ __volatile__ (
-	// delay 1 clock cycle.
-	".macro delay1\n\t"
-		"nop\n"
-	".endm\n"
-	
-	// delay 2 clock cycles
-	".macro delay2\n\t"
-		"nop\n\t"
-		"nop\n"
-	".endm\n"
-	
-	// delay 3 clock cyles
-	".macro delay3\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n"
-	".endm\n"
-	
-	// delay 4 clock cylces
-	".macro delay4\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n"
-	".endm\n"
-	
-	// delay 5 clock cylces
-	".macro delay5\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n"
-	".endm\n"
-	
-	// delay 6 clock cylces
-	".macro delay6\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n"
-	".endm\n"
-	
-	// delay 7 clock cylces
-	".macro delay7\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n"
-	".endm\n"
-	
-	// delay 8 clock cylces
-	".macro delay8\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n"
-	".endm\n"
-	
-	// delay 9 clock cylces
-	".macro delay9\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n"
-	".endm\n"
-	
-	// delay 10 clock cylces
-	".macro delay10\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n"
-	".endm\n"
-); // end of delay macros
+#if defined(__AVR_ATmega4809__)
 
-// common output macros, specific output macros at top of file
+// --- Nano Every (ATmega4809) @ 20MHz Version ---
+
 __asm__ __volatile__ (
-	
-	// save port 16 and clear the video bit
-	".macro svprt p\n\t"
-		"in		r16,\\p\n\t"
-		ANDI_HWS
-	".endm\n"
-	
-	// output 1 bit port safe
-	".macro o1bs p\n\t"
-		BLD_HWS
-		"out	\\p,r16\n"
-	".endm\n"
-); // end of output macros
+	".macro delay1\n\tnop\n.endm\n"
+	".macro delay2\n\tnop\n\tnop\n.endm\n"
+	".macro delay3\n\tnop\n\tnop\n\tnop\n.endm\n"
+	".macro delay4\n\tnop\n\tnop\n\tnop\n\tnop\n.endm\n"
+    ".macro delay5\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n.endm\n"
+    // VOUT is used for 1-cycle Virtual Port access on 4809
+	".macro svprt p\n\tin r16,\\p\n\tandi r16,~0x01\n.endm\n" 
+	".macro o1bs p\n\tbld r16,0\n\tvout \\p,r16\n.endm\n"
+);
+
+#else
+
+// --- Classic Arduino (328P/2560/etc) Version ---
+
+__asm__ __volatile__ (
+	".macro delay1\n\tnop\n.endm\n"
+	".macro delay2\n\tnop\n\tnop\n.endm\n"
+	".macro delay3\n\tnop\n\tnop\n\tnop\n.endm\n"
+	".macro delay4\n\tnop\n\tnop\n\tnop\n\tnop\n.endm\n"
+	".macro delay5\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n.endm\n"
+    ".macro delay6\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n.endm\n"
+    ".macro delay7\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n.endm\n"
+    ".macro delay8\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n.endm\n"
+    ".macro delay9\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n.endm\n"
+    ".macro delay10\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n.endm\n"
+);
+
+__asm__ __volatile__ (
+	".macro svprt p\n\tin r16,\\p\n\t" ANDI_HWS "\n.endm\n"
+	".macro o1bs p\n\t" BLD_HWS "\nout \\p,r16\n.endm\n"
+);
+
+#endif
+
 #endif
